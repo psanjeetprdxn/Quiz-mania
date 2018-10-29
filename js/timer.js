ce172fdc8ddb6513bf0dfb3ba6ac6b7e5daa99d7
sanjeet <sanjeetp@prdxn.com>
2018-10-29 15:38:53 +0530

    added countdown functionality

---
+++ js/timer.js
@@ -0,0 +1,20 @@
+var timeleft = 420;
+
+function timeout()
+{
+    var minutes = Math.floor(timeleft/60);
+    var seconds = timeleft%60;
+
+
+    // If the count down is over, write some text
+    if (timeleft < 0) {
+        clearInterval(x);
+        document.getElementById("auto-submit").submit();
+    }else{
+        document.getElementById('timer').value = timeleft;
+        document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+    }
+    timeleft--;
+    var x = setTimeout(function() {timeout()}, 1000);
+}
