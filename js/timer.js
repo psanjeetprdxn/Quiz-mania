ddd38f6727ecb4f7843f7d2da03f3e642ffafa18
sanjeet <sanjeetp@prdxn.com>
2018-10-30 17:23:11 +0530

    count-down timer wont reset after page refresh

--- js/timer.js
+++ js/timer.js
@@ -1,20 +1,21 @@
-var timeleft = 420;
+var totalTime = 420;  //In seconds
+		if (localStorage.getItem("timer") === null) {
+  			localStorage.setItem("timer", totalTime);
+		}
 
-function timeout()
-{
-    var minutes = Math.floor(timeleft/60);
-    var seconds = timeleft%60;
+		function timeout()
+		{
+			if (localStorage.timer < 0) {
 
-
-    // If the count down is over, write some text
-    if (timeleft < 0) {
-        clearInterval(x);
-        document.getElementById("auto-submit").submit();
-    }else{
-        document.getElementById('timer').value = timeleft;
-        document.getElementById("count-down").innerHTML = minutes + "m " + seconds + "s ";
-
-    }
-    timeleft--;
-    var x = setTimeout(function() {timeout()}, 1000);
-}
+    	    	localStorage.setItem("timer", totalTime);
+    	    	clearInterval(x);
+    	    	document.getElementById("auto-submit").submit();
+    		} else {
+    	    	minutes = Math.floor(localStorage.timer / 60);
+    	    	seconds = localStorage.timer % 60;
+    	    	document.getElementById("count-down").innerHTML = minutes + "m " + seconds + "s ";
+            document.getElementById("time").value = localStorage.timer;
+    		}
+    		localStorage.timer--;
+    		var x = setTimeout(function() {timeout()}, 1000);
+		}
