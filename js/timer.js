343e4b1bb4bfa87fe749e6aa9670837c143447ab
sanjeet <sanjeetp@prdxn.com>
2018-10-31 10:42:27 +0530

    indented code

--- js/timer.js
+++ js/timer.js
@@ -1,26 +1,26 @@
-var totalTime = 420;  //In seconds
-		if (localStorage.getItem("timer") === null) {
-  			localStorage.setItem("timer", totalTime);
-		}
+var totalTime = 420; //In seconds
 
-		function timeout()
-		{
-			if (localStorage.timer < 0) {
+if (localStorage.getItem("timer") === null) {
+    localStorage.setItem("timer", totalTime);
+}
 
-    	    	localStorage.setItem("timer", totalTime);
-    	    	clearInterval(x);
-    	    	document.getElementById("auto-submit").submit();
-    		} else {
-    	    	minutes = Math.floor(localStorage.timer / 60);
-    	    	seconds = localStorage.timer % 60;
-    	    	document.getElementById("count-down").innerHTML = minutes + "m " + seconds + "s ";
-            document.getElementById("time").value = localStorage.timer;
-    		}
-    		localStorage.timer--;
-    		var x = setTimeout(function() {timeout()}, 1000);
-		}
-
-    function resetTimerStorage()
-    {
-      localStorage.setItem("timer", totalTime);
+function timeout() {
+    if (localStorage.timer < 0) {
+        localStorage.setItem("timer", totalTime);
+        clearInterval(x);
+        document.getElementById("auto-submit").submit();
+    } else {
+        minutes = Math.floor(localStorage.timer / 60);
+        seconds = localStorage.timer % 60;
+        document.getElementById("count-down").innerHTML = minutes + "m " + seconds + "s ";
+        document.getElementById("time").value = localStorage.timer;
     }
+    localStorage.timer--;
+    var x = setTimeout(function() {
+    timeout()
+    }, 1000);
+}
+
+function resetTimerStorage() {
+    localStorage.setItem("timer", totalTime);
+}
